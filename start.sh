bfd7758d67711ecb746734dd7af3601d56a5e651
orginalmufurjp <102908457+orginalmufurjp@users.noreply.github.com>
2022-04-03 17:30:45 +0430

    Add files via upload

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/8wA9RRuGgnRQfr2/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/digibot3/VCPlayerBot /VCPlayerBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/8wA9RRuGgnRQfr2/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/digibot3/VCPlayerBot -b $BRANCH /VCPlayerBot
 fi
 cd /VCPlayerBot
 pip3 install -U -r requirements.txt
